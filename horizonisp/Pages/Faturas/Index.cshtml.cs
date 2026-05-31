ede39b8cc75fc32c88cff5e456f2a4b91f99f76b
mercadoria-capivariedades <jferreirarodrigues983@gmail.com>
2026-05-30 22:01:42 -0300

    2

--- horizonisp/Pages/Faturas/Index.cshtml.cs
+++ horizonisp/Pages/Faturas/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using horizonisp.Context;
+using horizonisp.Models.Enums;
 using horizonisp.Services;
 
 namespace horizonisp.Pages.Faturas
@@ -9,14 +10,25 @@ namespace horizonisp.Pages.Faturas
     public class IndexModel(IFaturamentoService faturamentoService, AppDbContext db) : PageModel
     {
         public IList<Models.Fatura> Faturas { get; private set; } = [];
+        public StatusFatura? FiltroStatus { get; private set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(StatusFatura? status)
         {
-            Faturas = await db.Faturas
+            FiltroStatus = status;
+
+            var query = db.Faturas
                 .Include(f => f.Assinatura)
                     .ThenInclude(a => a.Cliente)
                 .Include(f => f.Assinatura)
                     .ThenInclude(a => a.Plano)
+                .AsQueryable();
+
+            if (status.HasValue)
+            {
+                query = query.Where(f => f.Status == status.Value);
+            }
+
+            Faturas = await query
                 .OrderByDescending(f => f.DataVencimento)
                 .ToListAsync();
         }
